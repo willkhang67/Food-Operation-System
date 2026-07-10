fd7ef1eca7363006d9b534afcbdabc58b2d084d3
willlkhang <minhkhangnguyen31104@gmail.com>
2026-07-10 13:40:15 +0800

    entity: init all entities

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { FoodModule } from './food/food.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CategoryModule, FoodModule, OrderModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
