39f2ff4708eb15c645841c098d3ba8e7aaa9d20d
Phong <dinhphong2222004@gmail.com>
2026-07-14 18:01:04 +0700

    feat: implement Category entity with properties and decorators (add timestamp, status default 1)

--- backend/src/category/entities/category.entity.ts
+++ backend/src/category/entities/category.entity.ts
@@ -1,1 +1,30 @@
-export class Category {}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, } from 'typeorm';
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+    unique: true
+  })
+  name!: string;
+
+  @Column({
+  type: 'text',
+  nullable: true,
+  })
+  description!: string | null;
+
+  @Column({ type: 'int', default: 1 })
+  status!: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt!: Date;
+}
