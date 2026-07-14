732b774cdf304eaec5a2ade42859d9d67325a0ea
Phong <dinhphong2222004@gmail.com>
2026-07-14 17:59:14 +0700

    feat: add DTOs for category management

---
+++ backend/src/category/dto/category-response.dto.ts
@@ -0,0 +1,8 @@
+export class CategoryResponseDto {
+  id!: string;
+  name!: string;
+  description!: string | null;
+  status!: number;
+  createdAt!: Date;
+  updatedAt!: Date;
+}
--- backend/src/category/dto/create-category.dto.ts
+++ backend/src/category/dto/create-category.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCategoryDto {}
+import { IsOptional, IsString, MinLength, IsNotEmpty, MaxLength } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Name field cannot be empty' })
+  // @MinLength(1)
+  @MaxLength(50)
+  name!: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
