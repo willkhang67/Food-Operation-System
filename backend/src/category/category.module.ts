266bec4fc3f26feb3c3bb65af2164c259176b485
Phong <dinhphong2222004@gmail.com>
2026-07-17 16:57:39 +0700

    feat: update Category and Food modules to include FoodImage service and enhance TypeOrm imports

--- backend/src/category/category.module.ts
+++ backend/src/category/category.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+
 import { Category } from './entities/category.entity';
+import { Food } from '../food/entities/food.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category, Food])],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
--- backend/src/food/food.module.ts
+++ backend/src/food/food.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { FoodImageService } from './food-image.service';
 import { FoodService } from './food.service';
 import { FoodController } from './food.controller';
 
+import { Food } from './entities/food.entity';
+import { Category } from '../category/entities/category.entity';
+import { FoodImage } from './entities/food-image.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Food, Category, FoodImage])],
   controllers: [FoodController],
-  providers: [FoodService],
+  providers: [FoodService, FoodImageService],
+  exports: [TypeOrmModule],
 })
 export class FoodModule {}
+
