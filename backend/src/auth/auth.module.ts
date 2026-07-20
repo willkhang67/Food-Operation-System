3a4cb43b7969715d8ca2d92c7274d307acfbf972
willlkhang <minhkhangnguyen31104@gmail.com>
2026-07-20 21:17:38 +0800

    authen: minor modification

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
+//config authen
 @Module({
   imports: [
     UserModule,
