e68a90764d0c9205e2a6df77e6a72f8993cab0c9
willlkhang <minhkhangnguyen31104@gmail.com>
2026-07-23 19:35:51 +0800

    gaurd: fix the login post method throttle

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -6,12 +6,12 @@ import { LoginDto } from './dto/login.dto';
 import { Throttle } from '@nestjs/throttler';
 
 @Controller('auth')
-@Throttle({ auth: { ttl: 60_000, limit: 5 } }) // in app.modules.ts has the define for auth in throttle
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @Throttle({ auth: { ttl: 60_000, limit: 5 } }) // in app.modules.ts has the define for auth in throttle
   login(@Body() loginDto: LoginDto): Promise<AuthResponseDto> {
     return this.authService.login(loginDto);
   }
