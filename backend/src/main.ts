e0f975b0dcab0caed7c7c8592a8ff09d662b6501
willlkhang <minhkhangnguyen31104@gmail.com>
2026-07-28 15:35:27 +0800

    order: finish test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 6000);
 }
 void bootstrap();
