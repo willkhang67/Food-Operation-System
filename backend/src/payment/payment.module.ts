748907f39adf710f9436b23b4152cbb730e2f52e
willlkhang <minhkhangnguyen31104@gmail.com>
2026-07-29 22:05:12 +0800

    payment: register it in payment module

--- backend/src/payment/payment.module.ts
+++ backend/src/payment/payment.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderModule } from './../order/order.module';
+import { Order } from './../order/entities/order.entity';
+import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { StripeProvider } from './stripe/stripe.provider';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Payment, Order]),
+    OrderModule,
+  ],
   controllers: [PaymentController],
-  providers: [PaymentService],
+  providers: [PaymentService, StripeProvider],
+  exports: [PaymentService],
 })
 export class PaymentModule {}
