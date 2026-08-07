9cc50b7dc99386cbfca6ca232468fbe8dfb9051e
willlkhang <minhkhangnguyen31104@gmail.com>
2026-08-07 22:32:47 +0800

    feat: extent jst payload interface

--- backend/src/auth/interfaces/jwt-payload.interface.ts
+++ backend/src/auth/interfaces/jwt-payload.interface.ts
@@ -3,4 +3,6 @@ import { UserRole } from '../../user/enums/user-role.enum';
 export interface JwtPayload {
   sub: string;
   role: UserRole;
+  typ: 'access' | 'refresh';
+  jti: string; // requird on refresh
 }
