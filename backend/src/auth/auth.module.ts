060027b1f6dc603cfb33bc671187cc4158b7879b
willlkhang <minhkhangnguyen31104@gmail.com>
2026-08-07 22:31:39 +0800

    feat: register hasher to auth modules

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,11 +6,14 @@ import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RefreshToken } from './entities/refresh-token.entity';
 
 //config authen
 @Module({
   imports: [
     UserModule,
+    TypeOrmModule.forFeature([RefreshToken]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
